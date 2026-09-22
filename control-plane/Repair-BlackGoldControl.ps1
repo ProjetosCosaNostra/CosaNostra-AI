76fed1068200a6bc397c39ceb08315220e664fe1
Felipe Rosa Gomes <projetoscosanostra@gmail.com>
2026-09-22 15:52:04 -0300

    security(control-plane): pin repair to immutable stable commit

--- control-plane/Repair-BlackGoldControl.ps1
+++ control-plane/Repair-BlackGoldControl.ps1
@@ -1,5 +1,14 @@
 $ErrorActionPreference = 'Stop'
+
+$Repository = 'ProjetosCosaNostra/CosaNostra-AI'
 $StableRef = 'control-plane-stable'
-$BootstrapUrl = 'https://raw.githubusercontent.com/ProjetosCosaNostra/CosaNostra-AI/' + $StableRef + '/control-plane/bootstrap.ps1'
-$source = (Invoke-WebRequest -UseBasicParsing -Uri ($BootstrapUrl + '?repair=' + [DateTimeOffset]::UtcNow.ToUnixTimeSeconds())).Content
-& ([ScriptBlock]::Create($source))
+$Headers = @{ 'User-Agent' = 'BlackGold-ControlPlane/1.6' }
+$nonce = [DateTimeOffset]::UtcNow.ToUnixTimeSeconds()
+
+$branchInfo = Invoke-RestMethod -UseBasicParsing -Headers $Headers -Uri ('https://api.github.com/repos/' + $Repository + '/branches/' + $StableRef + '?t=' + $nonce)
+$StableCommit = [string]$branchInfo.commit.sha
+if ($StableCommit -notmatch '^[0-9a-f]{40}$') { throw 'Could not resolve immutable stable commit.' }
+
+$BootstrapUrl = 'https://raw.githubusercontent.com/' + $Repository + '/' + $StableCommit + '/control-plane/bootstrap.ps1'
+$source = (Invoke-WebRequest -UseBasicParsing -Uri ($BootstrapUrl + '?repair=' + $nonce)).Content
+& ([ScriptBlock]::Create($source)) -PinnedCommit $StableCommit
