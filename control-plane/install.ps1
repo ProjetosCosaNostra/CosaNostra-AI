19c8d78ffc21266d9b3f31a356bf509228c62d70
Felipe Rosa Gomes <projetoscosanostra@gmail.com>
2026-09-22 15:28:13 -0300

    release(control-plane): route installer through validated stable channel

--- control-plane/install.ps1
+++ control-plane/install.ps1
@@ -1,6 +1,8 @@
 $ErrorActionPreference = 'Stop'
-$Base = 'https://raw.githubusercontent.com/ProjetosCosaNostra/CosaNostra-AI/main/control-plane'
+$StableRef = 'control-plane-stable'
+$Base = 'https://raw.githubusercontent.com/ProjetosCosaNostra/CosaNostra-AI/' + $StableRef + '/control-plane'
 $nonce = [DateTimeOffset]::UtcNow.ToUnixTimeSeconds()
+
 $latest = Invoke-RestMethod -UseBasicParsing -Uri ($Base + '/LATEST.json?t=' + $nonce)
 $bootstrapSource = (Invoke-WebRequest -UseBasicParsing -Uri ($latest.bootstrap_url + '?v=' + $latest.version + '&t=' + $nonce)).Content
 & ([ScriptBlock]::Create($bootstrapSource))
